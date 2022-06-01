27506d7ebe1dea5b4ef7eb9ad4f9b32de8da8a5c
Mac <mac1@Macs-MacBook-Pro.local>
2022-06-01 10:06:09 +0200

    first commit

---
+++ dnb/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+  return <Component {...pageProps} />
+}
+
+export default MyApp
